941a9dd15cc8886f1900190d31b5465a1e0c6b58
Mykola Panko <mpanko@softserveinc.com>
2021-08-26 16:32:05 +0300

    hotfix

--- src/test/java/Hometask2.java
+++ src/test/java/Hometask2.java
@@ -2,52 +2,30 @@ import java.util.ArrayList;
 
 public class Hometask2 {
 
-    private static void printElementsByIndex(int indexOfElement1, int indexOfElement2, int listSize) {
-        ArrayList list1 = new ArrayList();
-        for (int element = 0; element <= listSize; element++) {
-            list1.add(element, "str" + ((int) (Math.random() * 10)));
-        }
-        System.out.println(list1);
-        if (indexOfElement2 < listSize) {
-            System.out.println("element1 = " + list1.get(indexOfElement1));
-            System.out.println("element2 = " + list1.get(indexOfElement2));
+    private static void printElementsByIndex(ArrayList list, int indexOfElement1, int indexOfElement2) {
+        if (indexOfElement2 <= list.size()) {
+            System.out.println("element1 = " + list.get(indexOfElement1));
+            System.out.println("element2 = " + list.get(indexOfElement2));
         } else {
-            System.out.println("Size of the list is less than index of the element2 = " + list1.size() + ". Index of the second element is incorrect");
+            System.out.println("Size of the list is less than index of the element2, size=" + list.size() + ". Index of the second element is incorrect");
         }
     }
 
-    private static void printReverseIntegerList(int intSize) {
-        ArrayList intList = new ArrayList();
-        while (intList.size() < intSize) {
-            intList.add((int) (Math.random() * 10));
-        }
-        System.out.println(intList);
+    private static void printReverseIntegerList(ArrayList intList) {
         for (int i = intList.size() - 1; i >= 0; i--) {
             System.out.print(intList.get(i) + " ");
         }
     }
 
-    private static void combineAndPrintTwoLists(int listSize1, int listSize2) {
-        ArrayList<String> list1 = new ArrayList<>();
-        for (int element = 0; element < listSize1; element++) {
-            list1.add(element, "str" + ((int) (Math.random() * 10)));
-        }
-        ArrayList<String> list2 = new ArrayList<>();
-        for (int element = 0; element < listSize2; element++) {
-            list2.add(element, "str" + ((int) (Math.random() * 10)));
-        }
+    private static void combineAndPrintTwoLists(ArrayList list1, ArrayList list2) {
         list1.addAll(list2);
         System.out.println("\n" + "Combined list = " + list1);
     }
 
-    private static double calculateSumOfElements(int size) {
-        ArrayList<Integer> intList = new ArrayList<>();
-        while (intList.size() <= 10) {
-            intList.add((int) (Math.random() * 10));
-        }
-        double sum = 0;
-        for (int i = 0; i < intList.size(); i++) {
-            sum = sum + intList.get(i);
+    private static double calculateSumOfElements(ArrayList<Integer> list) {
+        int sum = 0;
+        for (int i = 0; i < list.size(); i++) {
+            sum = sum + list.get(i);
         }
         return sum;
     }
@@ -66,31 +44,39 @@ public class Hometask2 {
         return newCombineString;
     }
 
-    public static ArrayList createList(int size) {
-        ArrayList list = new ArrayList<String>();
+    private static ArrayList createIntegerList(int size) {
+        ArrayList list = new ArrayList<Integer>();
+        for (int i = 0; i < size; i++) {
+            list.add(1 + i);
+        }
+        return list;
+    }
+
+    private static ArrayList createStringList(int size) {
+        ArrayList list = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             list.add("str" + i);
         }
         return list;
     }
 
-    public static ArrayList combineLists(ArrayList list1, ArrayList list2){
+    private static ArrayList combineLists(ArrayList list1, ArrayList list2) {
         ArrayList result = new ArrayList();
         result.addAll(list1);
         result.addAll(list2);
         return result;
     }
 
-    public static void printList(ArrayList list) {
+    private static void printList(ArrayList list) {
         System.out.println(list);
     }
 
     public static void main(String[] args) {
-        printElementsByIndex(1, 5, 10);
-        printReverseIntegerList(10);
-        combineAndPrintTwoLists(2, 4);
-        System.out.println(calculateSumOfElements(5));
+        printElementsByIndex(createStringList(10), 5, 10);
+        printReverseIntegerList(createIntegerList(10));
+        combineAndPrintTwoLists(createStringList(3), createStringList(5));
+        printList(combineLists(createStringList(7), createStringList(5)));
+        System.out.println(calculateSumOfElements(createIntegerList(6)));
         System.out.println(combineAllStrings("qwe1", "qwe", "gdf", "vcx", "dfg"));
-        printList(combineLists(createList(7), createList(5)));
     }
 }
