272d12c5782cb74c266d3907d557bb7ea45634b6
Mykola Panko <mpanko@softserveinc.com>
2021-09-14 18:47:48 +0300

    updates

--- src/test/java/Hometask2.java
+++ src/test/java/Hometask2.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Hometask2 {
 
-    private static void printElementsByIndex(ArrayList list, int indexOfElement1, int indexOfElement2) {
+    private static void printElementsByIndex(List<String> list, int indexOfElement1, int indexOfElement2) {
         if (indexOfElement2 <= list.size()) {
             System.out.println("element1 = " + list.get(indexOfElement1));
             System.out.println("element2 = " + list.get(indexOfElement2));
@@ -11,27 +12,27 @@ public class Hometask2 {
         }
     }
 
-    private static void printReverseIntegerList(ArrayList intList) {
+    private static void printReverseIntegerList(List<Integer> intList) {
         for (int i = intList.size() - 1; i >= 0; i--) {
             System.out.print(intList.get(i) + " ");
         }
     }
 
-    private static void combineAndPrintTwoLists(ArrayList list1, ArrayList list2) {
+    private static void combineAndPrintTwoLists(List<String> list1, List<String> list2) {
         list1.addAll(list2);
         System.out.println("\n" + "Combined list = " + list1);
     }
 
-    private static double calculateSumOfElements(ArrayList<Integer> list) {
+    private static double getSumOfElements(List<Integer> list) {
         int sum = 0;
-        for (int i = 0; i < list.size(); i++) {
-            sum = sum + list.get(i);
+        for (Integer aList : list) {
+            sum = sum + aList;
         }
         return sum;
     }
 
-    private static String combineAllStrings(String str1, String str2, String str3, String str4, String str5) {
-        ArrayList<String> list = new ArrayList<>();
+    private static String getAllStrings(String str1, String str2, String str3, String str4, String str5) {
+        List<String> list = new ArrayList<>();
         list.add(str1);
         list.add(str2);
         list.add(str3);
@@ -44,39 +45,39 @@ public class Hometask2 {
         return newCombineString;
     }
 
-    public static ArrayList createIntegerList(int size) {
-        ArrayList list = new ArrayList<Integer>();
+    public static List getIntegerList(int size) {
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             list.add(1 + i);
         }
         return list;
     }
 
-    private static ArrayList createStringList(int size) {
-        ArrayList list = new ArrayList<>();
+    private static List getStringList(int size) {
+        List<String> list = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             list.add("str" + i);
         }
         return list;
     }
 
-    private static ArrayList combineLists(ArrayList list1, ArrayList list2) {
-        ArrayList result = new ArrayList();
-        result.addAll(list1);
-        result.addAll(list2);
-        return result;
+    private static List getCombineLists(List<String> list1, List<String> list2) {
+        List<String> combinedList = new ArrayList<>();
+        combinedList.addAll(list1);
+        combinedList.addAll(list2);
+        return combinedList;
     }
 
-    private static void printList(ArrayList list) {
+    private static void printList(List list) {
         System.out.println(list);
     }
 
     public static void main(String[] args) {
-        printElementsByIndex(createStringList(10), 5, 9);
-        printReverseIntegerList(createIntegerList(10));
-        combineAndPrintTwoLists(createStringList(3), createStringList(5));
-        printList(combineLists(createStringList(7), createStringList(5)));
-        System.out.println(calculateSumOfElements(createIntegerList(6)));
-        System.out.println(combineAllStrings("qwe1", "qwe", "gdf", "vcx", "dfg"));
+        printElementsByIndex(getStringList(10), 5, 9);
+        printReverseIntegerList(getIntegerList(10));
+        combineAndPrintTwoLists(getStringList(3), getStringList(5));
+        printList(getCombineLists(getStringList(7), getStringList(5)));
+        System.out.println(getSumOfElements(getIntegerList(7)));
+        System.out.println(getAllStrings("qwe1", "qwe", "gdf", "vcx", "dfg"));
     }
 }
--- src/test/java/Hometask3.java
+++ src/test/java/Hometask3.java
@@ -3,35 +3,40 @@ import java.util.List;
 public class Hometask3 {
 
     // receiving list of numbers and print only even numbers
-    private static void printEvenNumbers (List list) {
-        for (int i = 0; i<=list.size()-1; i++){
-           if ((int) list.get(i)%2==0)
+    private static void printEvenNumbers (List<Integer> list) {
+        for (int i = 0; i<=list.size()-1; i++)
+        {
+           if (list.get(i)%2==0)
+           {
                System.out.print(list.get(i) + " ");
+           }
         }
     }
 
     // receiving list of number and changes places 1 and 3 element
-    private static void changeIndexOfElements(List list, int indexFirstElement, int indexSecondElement) {
-        int temp = (int) list.get(indexFirstElement);
+    private static void changeIndexOfElements(List<Integer> list, int indexFirstElement, int indexSecondElement) {
+        int temp = list.get(indexFirstElement);
         list.set(indexFirstElement, list.get(indexSecondElement));
         list.set(indexSecondElement, temp);
         System.out.println("\n" + list);
     }
 
     //receiving list of number and counts the sum of even numbers from 1 to 100
-    private static void printSumOfEvenNumbers (List list) {
+    private static void printSumOfEvenNumbers (List<Integer> list) {
         int sum = 0;
-        for (int i = 0; i <= list.size() - 1; i++) {
-            if ((int) list.get(i) % 2 == 0) {
-                sum = sum + (int) list.get(i);
+        for (int i = 0; i <= list.size() - 1; i++)
+        {
+            if (list.get(i) % 2 == 0)
+            {
+                sum = sum + list.get(i);
             }
         }
         System.out.println("Sum = " + sum);
     }
 
     public static void main(String[] args) {
-        printEvenNumbers(Hometask2.createIntegerList(8));
-        changeIndexOfElements(Hometask2.createIntegerList(3),0,2);
-        printSumOfEvenNumbers(Hometask2.createIntegerList(100));
+        printEvenNumbers(Hometask2.getIntegerList(8));
+        changeIndexOfElements(Hometask2.getIntegerList(3),0,2);
+        printSumOfEvenNumbers(Hometask2.getIntegerList(100));
     }
 }
