ff7ea94fcc9ac067661dd39c74d0d4161675c10b
mpanko1984 <punkfranuk@gmail.com>
2021-09-29 01:06:00 +0300

    hometask4

--- src/test/java/Employee.java
+++ src/test/java/Employee.java
@@ -1,7 +1,27 @@
-public abstract class Employee {
-    private String Name;
-    private String Surname;
-    private String ID;
 
-    abstract double getAverageMonthlySalary();
+
+public abstract class Employee {
+    private String name;
+    private String surname;
+    private int id;
+    public double salary;
+
+    public Employee() {
+    }
+
+    ;
+
+    public Employee(String name, String surname, int id, double salary) {
+        this.name = name;
+        this.surname = surname;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public abstract double[] getAverageMonthlySalary(int i);
+
+    @Override
+    public String toString() {
+        return name + " " + surname + " " + id + " " + salary;
+    }
 }
